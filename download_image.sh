32af256f4a63d76752702760ab46a2c7c7f2d7c0
Cedric Martens <cedricmartens98@gmail.com>
2020-09-04 13:31:14 -0400

    Fix download script

--- download_image.sh
+++ download_image.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# Arg1 : Port
+
+if [ -z "$1" ]; then
+  echo "Missing port. Check Freedom Robotics to get the port"
+  exit 1
+fi
 
 mkdir -p ~/rover_images
+echo "Password is root. If it doesn't work, you might need to enable SSH on FreedomRobotics. Make sure the port is correct."
 
-scp -P 10900 root@0.tunnel.freedomrobotics.ai:/catkin_ws/image_*.png ~/rover_images/
+scp -P $1 root@0.tunnel.freedomrobotics.ai:/catkin_ws/image_*.png ~/rover_images/
 
