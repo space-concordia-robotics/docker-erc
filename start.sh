a54257bba5d7e1eab4b0f020f91515a68b1efb4f
Cedric Martens <cedricmartens98@gmail.com>
2020-09-09 21:21:58 -0400

    Remove roslaunches

--- start.sh
+++ start.sh
@@ -8,14 +8,8 @@ echo "ROS_MASTER_URI=${ROS_MASTER_URI}" >> /etc/environment
 
 python /root/.local/lib/python2.7/site-packages/freedomrobotics/agent.py &
 
-roslaunch erc_example erc_bringup.launch &
 
-
-# CUSTOM SCRIPTS GO HERE
-# NOTE: YOU GOTTA LET SHIT SLEEP IN BETWEEN SO THAT IT ACTUALLY WORKS
-#sleep 4
-#roslaunch webcam_ar_track webcam_indiv.launch &
-#sleep 4
-#rosrun camera_vision ar_tracker.py &
+# PLEASE DON't PUT ROSLAUNCH FILES HERE
+# We want to see the output so we will just open another terminal to launch it.
 
 fg %1
